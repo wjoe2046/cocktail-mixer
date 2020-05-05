9d0b8ac8f02f77227a2aedaa4e4d81cdb867cc94
Wenfei Zhou <wzhou2046@gmail.com>
2020-05-05 06:07:29 -0400

    finished the project; needed caching and code splitting; will work on UI later

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -1,5 +1,28 @@
-import React from "react";
+import React from 'react';
+import Cocktail from './Cocktail';
 
-export default function CocktailList() {
-  return <h1>cocktailList component</h1>;
-}
+const CocktailList = ({ cocktails, loading }) => {
+  if (loading) {
+    return <h2 className='section-title'>Loading...</h2>;
+  }
+
+  if (cocktails.length < 1) {
+    return (
+      <h2 className='section-title'>
+        No cocktails matched your search criteria
+      </h2>
+    );
+  }
+  return (
+    <section className='section'>
+      <h2 className='section-title'>cocktails</h2>
+      <div className='cocktails-center'>
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />;
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default CocktailList;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,5 +1,37 @@
-import React from "react";
+import React from 'react';
 
-export default function SearchForm() {
-  return <h1>searchform component</h1>;
-}
+const SearchForm = ({ setSearchTerm }) => {
+  const searchRef = React.useRef('');
+
+  React.useEffect(() => {
+    searchRef.current.focus();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const searchCocktail = () => {
+    setSearchTerm(searchRef.current.value);
+  };
+
+  return (
+    <section className='section'>
+      <h2 className='section-title'>Search Cocktails</h2>
+      <form className='form search-form' onSubmit={handleSubmit}>
+        <div className='form-control'>
+          <label htmlFor='name'>search your favorite cocktail</label>
+          <input
+            type='text'
+            name='name'
+            id='name'
+            ref={searchRef}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default SearchForm;
